8c7e3213e3b3d96b026475652f3cd81d307e1198
Ntech <74048652+nilesh3938@users.noreply.github.com>
2021-05-26 08:23:20 +0530

    Add files via upload

---
+++ NodeMCU_Webserver.ino
@@ -0,0 +1,46 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid =  "nilesh";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "nilesh0000";
+
+WiFiClient client;
+WiFiServer server(80);
+
+#define led D5
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.begin("iot", "project1234");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print("..");
+  }
+  Serial.println();
+  Serial.println("NodeMCU is connected!");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  pinMode(led, OUTPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  client = server.available();  //Gets a client that is connected to the server and has data available for reading.    
+  if (client == 1)
+  {  
+    String request =  client.readStringUntil('\n');
+    Serial.println(request);
+    request.trim();
+    if(request == "GET /ledon HTTP/1.1")
+    {
+      digitalWrite(led, HIGH);
+    }
+    if(request == "GET /ledoff HTTP/1.1")
+    {
+      digitalWrite(led, LOW);
+    }
+  }
+}
